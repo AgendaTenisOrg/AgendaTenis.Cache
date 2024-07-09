06e28917ff1099eb0a8c90ddefa91398c90c7c27
matheuscm (Matheus Campanini) <matheus.campanini@ploomes.com>
2024-07-09 20:52:41 -0300

    Criado parâmetro lancarException

--- AgendaTenis.Cache.Core/RedisExtensoes.cs
+++ AgendaTenis.Cache.Core/RedisExtensoes.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
 
 namespace AgendaTenis.Cache.Core;
 
@@ -9,19 +10,43 @@ public static class RedisExtensoes
        string recordId,
        T data,
        TimeSpan? absoluteExpireTime = null,
-       TimeSpan? unusedExpireTime = null)
+       TimeSpan? unusedExpireTime = null,
+       bool lancarException = true)
     {
         var options = new DistributedCacheEntryOptions();
         options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
         options.SlidingExpiration = unusedExpireTime;
 
         var jsonData = System.Text.Json.JsonSerializer.Serialize(data);
-        await cache.SetStringAsync(recordId, jsonData, options);
+
+        try
+        {
+            await cache.SetStringAsync(recordId, jsonData, options);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Não foi possível adicionar registro ao cache. {ex}");
+
+            if (lancarException)
+                throw;
+        }
     }
 
-    public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+    public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId, bool lancarException = true)
     {
-        var jsonData = await cache.GetStringAsync(recordId);
+        string? jsonData = null;
+
+        try
+        {
+            jsonData = await cache.GetStringAsync(recordId);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Não foi possível obter registro do cache. {ex}");
+
+            if (lancarException)
+                throw;
+        }
 
         if (jsonData is null)
             return default;
